8be682448c0d3729682c7295781178ab68b9378f
hoang huy <56930653+turtleyuh@users.noreply.github.com>
2021-03-23 14:23:00 +0700

    Update ProductScreen.js
    add parameter, video, catalog

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -56,23 +56,23 @@ export default function ProductScreen(props) {
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
         <div>
-          <Link to="/">Back to result</Link>
+          <Link to="/">Trở lại</Link>
           <div className="row top">
-            <div className="col-2">
+            <div className="col-2 col">
               <img
                 className="large"
                 src={product.image}
                 alt={product.name}
               ></img>
             </div>
-            <div className="col-1">
+            <div className="col-1 col">
               <ul>
                 <li>
                   <h1>{product.name}</h1>
                 </li>              
                 <li>Giá : {product.price} VNĐ</li>
                 <li>
-                  Mô tả:
+                  Model:
                   <p>{product.description}</p>
                 </li>
                 <li>
@@ -80,18 +80,9 @@ export default function ProductScreen(props) {
                 </li>
               </ul>
             </div>
-            <div className="col-1">
-              <div className="card card-body">
-                <ul>
-                  <li>
-                    Seller{' '}
-                    <h2>
-                      <Link to={`/seller/${product.seller._id}`}>
-                        {product.seller.seller.name}
-                      </Link>
-                    </h2>
-                   
-                  </li>
+            <div className="col-1 col">
+              <div className="card productcard-body productscreen-card">
+                <ul>              
                   <li>
                     <div className="row">
                       <div>Giá</div>
@@ -100,12 +91,12 @@ export default function ProductScreen(props) {
                   </li>
                   <li>
                     <div className="row">
-                      <div>Status</div>
+                      <div>Trạng thái</div>
                       <div>
                         {product.countInStock > 0 ? (
-                          <span className="success">In Stock</span>
+                          <span className="success">Còn hàng</span>
                         ) : (
-                          <span className="danger">Unavailable</span>
+                          <span className="danger">Hết hàng</span>
                         )}
                       </div>
                     </div>
@@ -136,7 +127,7 @@ export default function ProductScreen(props) {
                           onClick={addToCartHandler}
                           className="primary block"
                         >
-                          Add to Cart
+                          Thêm vào giỏ hàng
                         </button>
                       </li>
                     </>
@@ -158,13 +149,14 @@ export default function ProductScreen(props) {
             </li>
           </ul>
           <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
-            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">{product.parameter}</div>
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">{product.video}</div>
+            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">{product.catalog}</div>
           </div>
           </div>
         </div>
       )}
+     
     </div>
   );
 }
