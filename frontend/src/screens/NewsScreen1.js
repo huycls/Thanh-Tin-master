a77e899bf23283ba1d7fc1df8b16072dc7bd1632
hoang huy <56930653+turtleyuh@users.noreply.github.com>
2021-03-23 09:50:17 +0700

    Add files via upload

---
+++ frontend/src/screens/NewsScreen1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NewsScreen1(props){
+    return <div className="newspage">
+        <h1>THÔNG BÁO THAY ĐỔI TÊN GIAO DỊCH</h1>
+        <div className="news-content">
+            <p>Thông báo thay đổi tên giao dịch: Kể từ ngày 29/07/13 Công ty TNHH TM DV XD Kỹ Thuật Thành Tín sẽ thay bằng tên Công ty TNHH Thiết Bị và Hóa Chất Thành Tín. Xin thông báo đến Quý khách hàng cũng như các đơn vị có giao dịch với công ty được rỏ.</p>
+            <p>Ngoài ra, Công ty Thành Tín sẽ chính thức đầu tư kinh doanh thêm vào mặt hàng phụ gia, hóa chất công nghiệp và hóa chất thí nghiệm để ngày càng phục vụ tốt hơn cho khách hàng trong cùng lĩnh vực ngành nghề kinh doanh.</p>
+            <p>(Theo GPKD được cấp lại lần 2 vào ngày 29/07/2013 của Sở Kế Hoạch và Đầu Tư Tp.HCM).</p>
+        </div>
+    </div>
+}
+export default NewsScreen1;
---
+++ frontend/src/screens/NewsScreen3.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+function NewsScreen3(props){
+    return <div className="newspage">
+        <h1>TUYỂN NHÂN VIÊN KINH DOANH</h1>
+        <small>15:11 16-1-2014</small>
+        <div className="news-content">
+            <p>
+            1/ Nhân viên kinh doanh thiết bị thí nghiệm, thiết bị đo lường:
+
+            Vị trí: Nhân viên kinh doanh
+            Giới tính: Nam
+            Tốt nghiệp: Đại học/Cao Đẳng
+            Chuyên ngành: Điện/ Điện Tử/ Tự Động Hóa/ Hóa
+            Tiếng Anh trình độ B trở lên, có khả năng đọc dịch các tài liệu kỹ thuật để tìm hiểu sản phẩm
+            Công việc: phụ trách tư vấn, bán hàng cho các nhà máy Dược phẩm, Thực phẩm, Bao Bì
+            Yêu thích kinh doanh, có thể đi công tác tỉnh
+            Ưu tiên: người có kinh nghiệm bán hàng thiết bị hoặc làm kỹ thuật
+            Lương : 4 triệu ~ 10 triệu đồng/tháng
+            Số lượng tuyển dụng: 3 người
+            2/ Nhân viên kinh doanh thiết bị thí nghiệm, thiết bị đo lường:
+
+            Vị trí: Nhân viên kỹ thuật
+            Giới tính: Nam
+            Tốt nghiệp: Đại học/Cao Đẳng
+            Chuyên ngành: Điện/ Điện Tử/ Tự Động Hóa,..
+            Tiếng Anh trình độ B trở lên, có khả năng đọc dịch các tài liệu kỹ thuật để tìm hiểu sản phẩm, lắp đặt thiết bị
+            Công việc: phụ trách đi tư vấn thiết bị và lắp đặt và hướng dẩn sử dụng
+            Chấp nhận đi công tác tỉnh
+            Ưu tiên: người có kinh nghiệm bán hàng thiết bị hoặc làm kỹ thuật
+            Lương : 4 triệu ~ 10 triệu đồng/tháng
+            Số lượng tuyển dụng: 2 người
+            Thông tin chung:
+
+            Địa điểm làm việc: Tp.Hồ Chí Minh
+            Thời gian làm việc: Giờ hành chính từ T2 đến T6.
+            Thời gian thử việc: 2 tháng
+            Các chế độ khác/phụ cấp: BHXH, BHYT, BHTN, các khoản khác theo quy định của luật lao động và quy định của Công ty.
+            Yêu cầu hồ sơ:
+            oĐơn xin việc (ghi rõ quá trình làm việc và tham gia các hoạt động khác)
+            oSơ yếu lý lịch
+            oBản sao hộ khẩu và CMND (có công chứng)
+            oBằng cấp (có công chứng)
+            oBảng điểm tốt nghiệp
+            oHình 3x4 (1 tấm)
+            Địa điểm nộp hồ sơ: Công Ty TNHH TM DV XD Kỹ Thuật Thành Tín (số 481 Điện Biên Phủ, P.3, Q.3, HCM)
+            Hạn nộp hồ sơ: ngày 30/07/2013
+            </p>
+        </div>
+    </div>
+}
+export default NewsScreen3;
