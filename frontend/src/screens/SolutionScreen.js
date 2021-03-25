1edaf82bd8d368884e64e5955ed608e417015eda
hoang huy <56930653+turtleyuh@users.noreply.github.com>
2021-03-25 16:12:34 +0700

    Add files via upload

---
+++ frontend/src/screens/SolutionScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function SolutionScreen(props){
+    return <div className="newspage">
+        <h1>Giải pháp công nghệ tự động</h1>
+        <small>15-1-2020, 4:51 pm</small>
+        <div className="news-image">
+        <img
+                  src="./images/sidebar-img.png"
+                  alt="giai-phap-cong-nghe-tu-dong"
+                />
+        </div>
+        <div className="news-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a nunc tempus, mattis ante interdum, volutpat ligula. Suspendisse mattis enim vel posuere aliquet. Duis magna ipsum, finibus non enim vel, dignissim molestie erat. Nulla felis neque, lacinia at est non, pharetra consequat mi. Suspendisse pharetra leo ac risus tempus sollicitudin. Sed at leo gravida, placerat mi at, hendrerit felis. Suspendisse nec tempus sem, ac feugiat tortor. Nam facilisis velit quis mollis porttitor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla condimentum, felis ac tristique fringilla, ante ex vestibulum nulla, sit amet placerat ex est lacinia mauris. Suspendisse potenti.</p>
+
+        <p>Morbi lorem lacus, lacinia ac venenatis a, bibendum eu eros. Nunc euismod molestie scelerisque. Aenean magna odio, semper vitae urna luctus, fringilla scelerisque ex. Donec dictum quam a purus pretium, at porttitor diam posuere. Vestibulum congue ligula diam, imperdiet eleifend justo fringilla sed. Sed maximus id augue eget fringilla. Nullam nunc purus, semper et gravida commodo, feugiat sit amet erat. Donec rutrum ante id blandit dapibus. Fusce vel diam euismod, sodales tortor nec, tempor turpis. Aenean lacinia tellus id arcu molestie tincidunt. In molestie urna nisi, in eleifend odio facilisis eu. Nullam hendrerit, diam sed interdum cursus, magna mauris luctus eros, eget auctor nunc justo id enim. Sed sit amet felis eu est molestie rutrum id vitae erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis est augue, pretium ac viverra et, eleifend in diam. Nullam suscipit luctus mattis.</p>
+
+        <p>Sed fermentum orci magna, eget sagittis quam faucibus id. Proin feugiat interdum tincidunt. Cras malesuada arcu ut neque commodo commodo. Nunc non nulla vehicula elit rhoncus dictum. Vestibulum ac eros leo. Nunc elementum ligula tortor, vitae blandit quam auctor nec. In fringilla a libero et commodo. Vivamus magna ipsum, posuere eu dignissim id, rutrum et turpis. Praesent mollis nec nunc non rutrum. Praesent tempus neque tortor, sit amet pulvinar nunc tristique vitae. Duis a pharetra risus, rhoncus euismod mauris. Nullam tempor ex sollicitudin, consequat felis vitae, tristique ipsum. Praesent ex diam, congue non quam et, auctor condimentum nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam, metus quis efficitur mollis, sem mauris congue dui, id pretium ex orci ac felis. Nam scelerisque et leo et volutpat.</p>
+
+        <p>Curabitur ut ornare dui, at eleifend nisl. Aliquam urna erat, iaculis ut mollis vel, convallis in lectus. Nullam ac aliquet massa. Praesent congue mi eget lectus lobortis porttitor. Maecenas in ipsum feugiat arcu tincidunt malesuada vel ut magna. In porta ullamcorper metus, vitae luctus mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi velit lectus, consequat nec odio quis, aliquam ultricies ligula. Proin bibendum quam eu justo pulvinar, et ornare neque vestibulum. Vestibulum ipsum nunc, fringilla vitae neque eu, molestie luctus risus.</p>
+
+        <p>Sed eget hendrerit metus. Etiam mattis sem molestie diam volutpat, faucibus sodales metus pretium. Integer non quam et nisi imperdiet tristique sit amet elementum mi. Mauris luctus sagittis laoreet. Nulla a sagittis dolor, a maximus metus. Aenean sagittis egestas neque, a sagittis nunc venenatis et. Donec et luctus diam, et aliquam purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla pellentesque sagittis consequat. Sed lobortis pellentesque erat at gravida. Praesent elit odio, sagittis eget consequat et, accumsan nec massa.</p>
+        </div>
+    </div>
+}
+export default SolutionScreen;
