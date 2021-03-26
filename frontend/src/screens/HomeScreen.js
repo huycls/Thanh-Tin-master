87092c4d8be4b1928ecb00e056298d3fef0eebee
hoang huy <56930653+turtleyuh@users.noreply.github.com>
2021-03-26 16:10:22 +0700

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -28,69 +28,33 @@ export default function HomeScreen() {
   return (
     <div>
       <div className="upper-content">
-        <div
-          id="carouselExampleIndicators"
-          className="carousel slide"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-          </div>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img
-                src="./images/giaiphap-slide.jpg"
-                className="d-block w-100"
-                alt="..."
-              />
+        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="2000">
+              <img src="./images/3.jpg" class="d-block w-100" alt="..." />
             </div>
-            <div className="carousel-item">
-              <img src="./images/3.jpg" className="d-block w-100" alt="..." />
+            <div class="carousel-item" data-bs-interval="2000">
+              <img src="./images/4.jpg" class="d-block w-100" alt="..." />
             </div>
-            <div className="carousel-item">
-              <img src="./images/4.jpg" className="d-block w-100" alt="..." />
+            <div class="carousel-item">
+              <img src="./images/thiet-bi-ptn.jpg" class="d-block w-100" alt="..." />
             </div>
           </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev"
-          >
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
           </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next"
-          >
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
           </button>
         </div>
         <div className="news">
-          <div className="news-title">Tin mới</div>
+          <div className="news-title"><i className="fas fa-chevron-right"></i> <Link to="/intro">Giới thiệu</Link></div>
+          <div className="news-content">
+            <strong><i className="fas fa-chevron-right"></i>CÔNG TY TNHH THIẾT BỊ VÀ HÓA CHẤT THÀNH TÍN</strong> hoạt động trong lĩnh vực tư vấn, cung cấp các giải pháp, thiết bị phòng thí nghiệm, thiết bị công nghiệp, hóa chất, vật tư...
+          </div>
+          <div className="news-title"><i className="fas fa-chevron-right"></i> <Link to="/allnews">Tin mới</Link></div>
           <div className="news-content">
             <Link to="/news">
               <p>
@@ -101,7 +65,7 @@ export default function HomeScreen() {
             </Link>
             <Link to="news1">
               <p>
-                <i className="fas fa-chevron-right"></i>THÔNG BÁO THAY ĐỔI TÊN GIAO DỊCH
+                <i className="fas fa-chevron-right"></i>Thông báo thay đổi tên giao dịch
               </p>
             </Link>
             <Link to="news2">
@@ -111,7 +75,7 @@ export default function HomeScreen() {
             </Link>
             <Link to="news3">
               <p>
-                <i className="fas fa-chevron-right"></i>TUYỂN NHÂN VIÊN KINH DOANH
+                <i className="fas fa-chevron-right"></i>Tuyển nhân viên kinh doanh
               </p>
             </Link>
             
@@ -129,7 +93,7 @@ export default function HomeScreen() {
             <div>
               {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
               <div className="rowe">
-                {products.map((product) => (
+                {products.filter(item => item.name).map((product) => (
                   <Product key={product._id} product={product}></Product>
                 ))}
               </div>
@@ -180,49 +144,7 @@ export default function HomeScreen() {
                 </span>
               </Link>
             </div>
-            <div className="sidebar-title">
-              <Link to="/solution">Giải pháp công nghệ tự động</Link>
-            </div>
-            <div className="sidebar-content">
-              <Link to="/solution" className="content-img">
-                <img
-                  src="./images/sidebar-img.png"
-                  alt="giai-phap-cong-nghe-tu-dong"
-                />
-                <div className="caption">
-                  <h5> Lorem ipsum dolor sit amet.</h5>
-                  <p>15-1-2020, 4:51 pm</p>
-                </div>
-              </Link>
-            </div>
-            <div className="sub-content">
-              <Link to="/nproduct">
-                <img
-                  src="./images/thumb_DM4A300.jpg"
-                  alt="thiet-bi-phong-thi-nghiem"
-                />
-                <span>
-                  {" "}
-                  Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet,
-                  consectetur adipiscing elit. Maecenas eu nulla malesuada,
-                  vehicula eros ac
-                </span>
-              </Link>
-            </div>
-            <div className="sub-content">
-              <Link to="/nproduct">
-                <img
-                  src="./images/thumb_DM4A300.jpg"
-                  alt="thiet-bi-phong-thi-nghiem"
-                />
-                <span>
-                  {" "}
-                  Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet,
-                  consectetur adipiscing elit. Maecenas eu nulla malesuada,
-                  vehicula eros ac
-                </span>
-              </Link>
-            </div>
+           
         </div>
       </div>  
     </div>
