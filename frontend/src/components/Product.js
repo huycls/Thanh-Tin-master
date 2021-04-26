d1e36a5724198f34199d9bbb20eacfdcf8b2b16d
hoang huy <hoanghuycls@gmail.com>
2021-04-26 08:42:00 +0700

    26/4 1st commit

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -59,7 +59,7 @@ export default withNamespaces((props) => props.namespaces) (function Product(pro
       </Link>
       <div className="card-body">
         <Link to={`/san-pham/${product._id}`} className="product-name">
-          <h2>{t("cutname", {product})}</h2>
+          <h2>{t("cutname", {product}).length > 50 ? t("cutname", {product}).substring(0, 47) + "..." : t("cutname", {product})  }</h2>
         </Link>
         <div className="row">
           <div className="product-model"><strong>Model: </strong> {product.model}</div>
